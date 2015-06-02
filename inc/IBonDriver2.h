26b0a585a63d7566d4c4eed244a7152f3431fbae
unknown <unknown_@live.jp>
2015-06-02 19:58:08 +0900

    特に意味無し…

--- inc/IBonDriver2.h
+++ inc/IBonDriver2.h
@@ -20,15 +20,15 @@ public:
 	virtual LPCTSTR GetTunerName(void) = 0;
 
 	virtual const BOOL IsTunerOpening(void) = 0;
-	
+
 	virtual LPCTSTR EnumTuningSpace(const DWORD dwSpace) = 0;
 	virtual LPCTSTR EnumChannelName(const DWORD dwSpace, const DWORD dwChannel) = 0;
 
 	virtual const BOOL SetChannel(const DWORD dwSpace, const DWORD dwChannel) = 0;
-	
+
 	virtual const DWORD GetCurSpace(void) = 0;
 	virtual const DWORD GetCurChannel(void) = 0;
-	
+
 // IBonDriver
 	virtual void Release(void) = 0;
 };
